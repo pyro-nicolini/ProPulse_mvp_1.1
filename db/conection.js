8efbf6f7bd13e2fdd107dc85e8c013bc07985aed
Pyro Nicolini <178456562+pyro-nicolini@users.noreply.github.com>
2025-10-05 00:22:42 -0300

    fix pool

--- db/conection.js
+++ db/conection.js
@@ -2,20 +2,16 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  allowExitOnIdle: true,
-  client_encoding: "UTF8",
+let pool;
+
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
+pool
+  .connect()
+  .then(() => console.log("✅ Conectado a PostgreSQL"))
+  .catch((err) => console.error("❌ Error de conexión a PostgreSQL:", err));
+
 module.exports = pool;
-
-// Para la facilidad del ejercicio entre compañeros utilizamos esta configuración.
-
-// Sabemos que nunca se deben incluir estos datos sensibles en un proyecto real y utilizar la variable de entorno.
-
-
