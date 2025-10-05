328074e29741c4e592308a4cbea7b27ffd61ed99
Pyro Nicolini <178456562+pyro-nicolini@users.noreply.github.com>
2025-10-04 23:56:36 -0300

    packg json

--- db/conection.js
+++ db/conection.js
@@ -1,19 +1,21 @@
-const { Pool } = require('pg');
-
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '1234',
-  host: 'localhost',
-  database: 'propulse',
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   allowExitOnIdle: true,
-  client_encoding: 'UTF8'
+  client_encoding: "UTF8",
 });
 
 module.exports = pool;
 
-
-// Para la facilidad del ejercicio entre compañeros utilizamos esta configuración. 
+// Para la facilidad del ejercicio entre compañeros utilizamos esta configuración.
 
 // Sabemos que nunca se deben incluir estos datos sensibles en un proyecto real y utilizar la variable de entorno.
+
+
